2d8760799a1a60af5cf21099bf1d4c4933101058
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-02-28 10:07:31 -0300

    first commit

---
+++ src/main/java/com/wamk/studantregistration/StudantRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.wamk.studantregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudantRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudantRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wamk/studantregistration/StudantRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wamk.studantregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudantRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
