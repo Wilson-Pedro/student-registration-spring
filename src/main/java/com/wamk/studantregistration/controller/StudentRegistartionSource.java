e53f7135f9091eb625aa1e993ae48be006e124f3
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-02-28 16:25:04 -0300

    Model Student and Student source

---
+++ src/main/java/com/wamk/studantregistration/controller/StudentRegistartionSource.java
@@ -0,0 +1,11 @@
+package com.wamk.studantregistration.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student-registration")
+public class StudentRegistartionSource {
+	
+
+}
---
+++ src/main/java/com/wamk/studantregistration/models/Student.java
@@ -0,0 +1,128 @@
+package com.wamk.studantregistration.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_ESTUDENT")
+public class Student implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	
+	@Column(nullable = false, unique = false, length = 255)
+	private String name;
+	
+	@Column(nullable = false, unique = true, length = 9)
+	private String registration;
+	
+	@Column(nullable = false, unique = false)
+	private String period;
+	
+	@Column(nullable = false, unique = false)
+	private String course;
+	
+	@Column(nullable = false, unique = false)
+	private LocalDateTime registrationDate;
+	
+	public Student() {
+	}
+	
+	public Student(UUID id, String name, String registration, String period, String course,
+			LocalDateTime registrationDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.registration = registration;
+		this.period = period;
+		this.course = course;
+		this.registrationDate = registrationDate;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRegistration() {
+		return registration;
+	}
+
+	public void setRegistration(String registration) {
+		this.registration = registration;
+	}
+
+	public String getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public LocalDateTime getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(LocalDateTime registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
